f60a8eb059a6b0cb3aa01feb52b44f497554fed1
babystar-5 <running.c.12@gmail.com>
2025-07-18 20:19:03 +0900

    hi

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json());
+app.use(bodyParser.json());   
 
 const sessions = {}; // temporary memory
 
